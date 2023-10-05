dcd6473d6b35ba2d79ba43b0fcfdad225bd866e0
ericvanlooy <ericvanlooy@hotmail.com>
2023-10-05 10:59:32 +0100

    Add chosen selection values to win/lose alerts

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function playRound(playerSelection,computerSelection){
     computerSelection = getComputerChoice(); /*get the string value returned from the getComputerChoice() function */
 
     if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock") { /*toLowerCase helps to match any capitalised input with lowercase rock, paper, or scissors, then we check if any of the 3 player selections beat the computer selection */
-        alert("You win"); /* announce the win*/
+        alert(`You win, ${playerSelection} beats ${computerSelection}`); /* announce the win*/
     } else if (playerSelection.toLowerCase() === computerSelection) { /*We check if any of the 3 player selections are the same as the computer selection */
         alert("It is a draw!"); /* announce the draw*/
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" || playerSelection.toLowerCase() === "scissors" && computerSelection === "rock" || playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") { /*We check if any of the 3 player selections are beaten by the computer selection */
-        alert("You lose"); /* announce the loss*/
+        alert(`You lose, ${computerSelection} beats ${playerSelection}`); /* announce the loss*/
     } else { /*Scenario if the player selection is not any of the above - due to wrong input*/
         alert(`That was not the right input, you chose ${playerSelection}`); /* announce the wrong input*/
 
